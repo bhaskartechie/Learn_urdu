b6a2bd4f13958cf33b0c69f9a351538d84c0a43c
Bhaskar R <bhaskar.techiepro@gmail.com>
2025-04-18 15:04:25 +0530

    feat: included test cases and testing in the ci-cd

--- urdu_lyrics/tests.py
+++ urdu_lyrics/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class HelloWorldViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_hello_world_response_status(self):
+        response = self.client.get('/hello/')  # Adjust the URL if necessary
+        self.assertEqual(response.status_code, 200)
+
+    def test_hello_world_response_content(self):
+        response = self.client.get('/hello/')  # Adjust the URL if necessary
+        self.assertEqual(response.content.decode(), "Hello World, Learn Urdu, checking the automate deployment process")
