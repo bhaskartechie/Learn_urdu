ccc051ec68193690cf6a803b5610ff3e83cc529e
Bhaskar R <bhaskar.techiepro@gmail.com>
2025-04-18 14:48:53 +0530

    Update hello_world response message to include deployment process check

--- urdu_lyrics/views.py
+++ urdu_lyrics/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse("Hello World, Learn Urdu!")
+    return HttpResponse("Hello World, Learn Urdu, checking the automate deployment process")
